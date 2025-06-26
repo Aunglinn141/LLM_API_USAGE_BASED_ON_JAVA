eadba1e6e9e2e1fa96d1c6aceefdae292698a15a
Aunglinn141 <131678986+Aunglinn141@users.noreply.github.com>
2025-06-26 09:08:27 +0800

    Update TestApiKey.java

--- yu-ai-agent/src/main/java/com/aung/yuaiagent/demo/invoke/TestApiKey.java
+++ yu-ai-agent/src/main/java/com/aung/yuaiagent/demo/invoke/TestApiKey.java
@@ -1,5 +1,5 @@
 package com.aung.yuaiagent.demo.invoke;
 
 public interface TestApiKey {
-        String API_KEY = "sk-2d138d462db944f6824f1875efdfda75";
+        String API_KEY = "YOUR_API_KEY";
 }
